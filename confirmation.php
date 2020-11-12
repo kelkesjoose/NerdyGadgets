244f70db30107aa7f42f0b6b981659f182908cbc
kelkesjoose <kaha-kelly@hotmail.com>
2020-11-12 15:12:42 +0100

    Aangemaakt

--- confirmation.php
+++ confirmation.php
@@ -2,22 +2,9 @@
 // zonder header?
 include "connect.php";
 ?>
-
 <!DOCTYPE html>
 <html lang="en" style="background-color: rgb(35,35,47);">
 <head>
-    <script src="Public/JS/fontawesome.js" crossorigin="anonymous"></script>
-    <script src="Public/JS/jquery.min.js"></script>
-    <script src="Public/JS/bootstrap.min.js"></script>
-    <script src="Public/JS/popper.min.js"></script>
-    <script src="Public/JS/Resizer.js"></script>
-    <script src="Public/JS/jquery-3.4.1.js"></script>
-    <style>
-        @font-face {
-            font-family: MmrText;
-            src: url(/Public/fonts/mmrtext.ttf);
-        }
-    </style>
     <meta charset="ISO-8859-1">
     <title>NerdyGadgets</title>
     <link rel="stylesheet" href="Public/CSS/Style.css" type="text/css">
@@ -39,17 +26,16 @@ include "connect.php";
     <link rel="manifest" href="Public/Favicon/manifest.json">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="Public/Favicon/ms-icon-144x144.png">
-    <meta name="theme-color" content="#ffffff">
-</head>
+    <meta name="theme-color" content="#ffffff"></head>
 <body>
-<div id="LogoImage"></div>
-<h2> Afrekenen</h2>
-<br>
-<br>
-<form method="get" action="confirmation.php">
-    <!-- overzicht artikelen? -->
-    Totaal aantal producten:<br><br> <!-- Hier producten teruglaten komen vanuit de cart? Weet niet hoe?-->
-    Totaal te betalen bedrag:<br><br><br><br><br> <!-- Hier bedragen terug laten komen vanuit de cart? Weet niet hoe?-->
-    <input type="submit" value="Betalen!" name="submit"><br>
-</form>
+<?php
+
+if(isset($_GET["submit"])){
+    print("Bedankt voor uw betaling!");
+} else {
+    print("Er is iets fout gegaan");
+}
+?>
 </body>
+</html>
+
