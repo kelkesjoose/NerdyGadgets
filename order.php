efc8b8a7214eff5eaa04e900480e0e4c6ab3ac56
Bwobl <christian.braamburg@gmail.com>
2020-11-25 13:54:49 +0100

    Bestelgegevens toegevoegd

--- order.php
+++ order.php
@@ -1,6 +1,13 @@
 <?php
 include "CartFunctions.php";
 include "header.php";
+include __DIR__ . "/connect.php";
+$totaalPrijs = 0;
+$teller = 0;
+
+$subtotaal = 0;
+$btwWaarde = 0;
+
 
 
 
@@ -9,9 +16,46 @@ if (empty($_SESSION["cart"])) {
 }
 
 else {
-  print_r($_SESSION["cart"]);
+    print('<h2>Bestelgegevens</h2><h5>');
+  foreach ($_SESSION["cart"] as $productnummer => $aantal) {
+      $teller++;
+
+      $query = "SELECT StockItemName, TaxRate, RecommendedRetailPrice, (RecommendedRetailPrice*(1+(TaxRate/100))) AS SellPrice
+       FROM StockItems
+       WHERE StockItemID = ?";
+
+      $Statement = mysqli_prepare($Connection, $query);
+      mysqli_stmt_bind_param($Statement, "i", $productnummer);
+      mysqli_stmt_execute($Statement);
+      $R = mysqli_stmt_get_result($Statement);
+      $R = mysqli_fetch_all($R, MYSQLI_ASSOC);
+
+      print '<td style="text-align: left">';
+      print ($R[0]["StockItemName"]);
+      print '</td>';
+      print '<td>';
+      print "$aantal";
+      print '</td>';
+      print '<td>';
+      print round(($R[0]["SellPrice"]), 2) * $aantal;
+      print '</td>';
+      print '<br>';
+      echo "<td style='text-align: left'></td></tr>";
+
+      $totaalPrijs = $totaalPrijs + (($R[0]["SellPrice"]) * $aantal);
+      $subtotaal = $subtotaal + ($R[0]["RecommendedRetailPrice"]);
+      $btwWaarde = ($R[0]["TaxRate"]) / 100 * $totaalPrijs;
+  }
+  print '<br>';
+  print '<br>';
+  $subtotalen = '';
+
+
+  $subtotalen .= "<p class='subtotalen'>Totaalprijs: €" . round(($totaalPrijs), 2) . "</p></td></tr>";
+
+  print $subtotalen;
+  print('</h5>');
   print('<body>
-    <h2>Bestelgegevens</h2>
     <br>
     <h3>Alle velden zijn verplicht!</h3><br>
     <form method="get" action="ordercontrol.php">
