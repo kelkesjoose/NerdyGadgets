dd5d1716d752795076e36d3bc1cf5bf9c589689f
kelkesjoose <kaha-kelly@hotmail.com>
2020-11-27 17:42:33 +0100

    Review opnieuw push wesley niet compleet.

--- reviewform.php
+++ reviewform.php
@@ -1,7 +1,25 @@
 <?php
 include __DIR__ . "/header.php";
 
-$ster = 0;
+$star = 0;
+$date = getdate();
+$ReviewID = "";
+
+foreach ($date as $time => $rv) {
+    if ($time == "mday") {
+        $day = $date["mday"];
+    }
+    if ($time == "mon") {
+        $month = $date["mon"];
+    }
+    if ($time == "year") {
+        $year = $date["year"];
+    break;
+    }
+}
+
+$ReviewDatum = "$year-$month-$day";
+print $ReviewDatum;
 
 ?>
 
@@ -14,21 +32,37 @@ $ster = 0;
 </head>
 
 <?php
-if ($_POST[star1] == "on") {
-    $star = 1;
-}
+// foreach ($_POST as $key => $value) {
+//     if ($key == "star1") {
+//         $star = $star+1;
+//         print $star;
+//     }
+//     if ($key == "star2") {
+//         $star = $star+1;
+//         print $star;
+//     }
+//     if ($key == "star3") {
+//         $star = $star+1;
+//         print $star;
+//     }
+//     if ($key == "star4") {
+//         $star = $star+1;
+//         print $star;
+//     }
+//     if ($key == "star5") {
+//         $star = $star+1;
+//         print $star;
+//     }
+// } 
 
-// $Query = "
-// INSERT INTO reviewproduct
-// VALUES ('', ?, ?, ?, ?);"
+$Query = "
+INSERT INTO reviewproduct
+VALUES (?, ?, ?, ?, ?)";
 
-// $Statement = mysqli_prepare($Connection, $Query);
-// mysqli_stmt_bind_param($Statement, "isiss", default, $_POST[OnderwerpReview],$_POST[ToelichtingReview],$_POST[star]);
-// mysqli_stmt_execute($Statement);
-// $R = mysqli_stmt_get_result($Statement);
-// $R = mysqli_fetch_all($R, MYSQLI_ASSOC);
-
-print_r ("$_POST");
+$Statement = mysqli_prepare($Connection, $Query);
+mysqli_stmt_bind_param($Statement, "issis", $ReviewID, $_POST[OnderwerpReview],$_POST[ToelichtingReview],$star,$ReviewDatum);
+mysqli_stmt_execute($Statement);
+$R = mysqli_stmt_get_result($Statement);
 ?>
 
 <body>
@@ -38,18 +72,19 @@ Naam: <input type="text" name="OnderwerpReview" placeholder="Onderwerp" class="T
     <div class="PaddingToelichtingReview">
 Toelichting:<br><textarea rows="5" cols="70" placeholder="Vul hier uw toelichting in" name="ToelichtingReview" class="ToelichtingReview"> </textarea>
     </div>
-    <div class="rating">
-        <input type="radio" name="star" id="star1"><label for="star1">
+    <!-- <div class="rating">
+        <input type="radio" name="star1" id="star1"><label for="star1">
         </label>
-        <input type="radio" name="star" id="star2"><label for="star2">
+        <input type="radio" name="star2" id="star2"><label for="star2">
         </label>
-        <input type="radio" name="star" id="star3"><label for="star3">
+        <input type="radio" name="star3" id="star3"><label for="star3">
         </label>
-        <input type="radio" name="star" id="star4"><label for="star4">
+        <input type="radio" name="star4" id="star4"><label for="star4">
         </label>
-        <input type="radio" name="star" id="star5"><label for="star5">
+        <input type="radio" name="star5" id="star5"><label for="star5">
         </label>
-    </div>
+    </div> -->
+    <input type="range" min="1" max="5" id="star" name="star" value >
 <input type="submit" value="Verstuur!" name="Verstuur" class="betaalbutton">
 </form>
 
