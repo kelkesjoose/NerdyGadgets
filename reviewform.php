1bdffa411b333d2a9299d52b1cb9efda25bf1a56
OfficialPorky <74242230+OfficialPorky@users.noreply.github.com>
2020-11-27 17:32:08 +0100

    Review Sectie bijna afgemaakt

--- reviewform.php
+++ reviewform.php
@@ -1,5 +1,8 @@
 <?php
 include __DIR__ . "/header.php";
+
+$ster = 0;
+
 ?>
 
 <!DOCTYPE html>
@@ -7,19 +10,34 @@ include __DIR__ . "/header.php";
 <head>
     <meta charset="UTF-8">
     <title> Review pagina </title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css
-    /font-awesome.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
+
+<?php
+if ($_POST[star1] == "on") {
+    $star = 1;
+}
+
+// $Query = "
+// INSERT INTO reviewproduct
+// VALUES ('', ?, ?, ?, ?);"
+
+// $Statement = mysqli_prepare($Connection, $Query);
+// mysqli_stmt_bind_param($Statement, "isiss", default, $_POST[OnderwerpReview],$_POST[ToelichtingReview],$_POST[star]);
+// mysqli_stmt_execute($Statement);
+// $R = mysqli_stmt_get_result($Statement);
+// $R = mysqli_fetch_all($R, MYSQLI_ASSOC);
+
+print_r ("$_POST");
+?>
 
 <body>
 <h1>Plaats review:</h1>
-<form method="get" action="reviewform.php">
+<form method="post" action="reviewform.php">
 Naam: <input type="text" name="OnderwerpReview" placeholder="Onderwerp" class="TextVeldReview" > <br><br>
     <div class="PaddingToelichtingReview">
-Toelichting:<br><textarea rows="5" cols="70" placeholder="Vul hier uw toelichting in" class="ToelichtingReview"> </textarea>
+Toelichting:<br><textarea rows="5" cols="70" placeholder="Vul hier uw toelichting in" name="ToelichtingReview" class="ToelichtingReview"> </textarea>
     </div>
-<input type="submit" value="Verstuur" name="Verstuur!" class="betaalbutton">
-</form>
     <div class="rating">
         <input type="radio" name="star" id="star1"><label for="star1">
         </label>
@@ -32,8 +50,7 @@ Toelichting:<br><textarea rows="5" cols="70" placeholder="Vul hier uw toelichtin
         <input type="radio" name="star" id="star5"><label for="star5">
         </label>
     </div>
-
-
-
+<input type="submit" value="Verstuur!" name="Verstuur" class="betaalbutton">
+</form>
 
 </body>
