2eaea05ed9f3c44536455075a72147050fd4c7c1
kelkesjoose <kaha-kelly@hotmail.com>
2020-12-02 13:59:22 +0100

    Review aangepast in View

--- view.php
+++ view.php
@@ -193,47 +193,22 @@ if ($R) {
                 <h1>Plaats review:</h1>
                 <form method="post" action="reviewform.php">
                     Naam: <input type="text" name="OnderwerpReview" placeholder="Onderwerp" class="TextVeldReview" > <br><br>
+                    <div class="paddingstar">
+                        <img src="public/img/Star3.png" width="40px" height="40px" id="paddingstar">
+                    <img src="public/img/Star3.png" width="40px" height="40px" id="paddingstar">
+                    <img src="public/img/Star3.png" width="40px" height="40px" id="paddingstar">
+                    <img src="public/img/Star3.png" width="40px" height="40px" id="paddingstar">
+                    <img src="public/img/Star3.png" width="40px" height="40px" id="paddingstar">
+                    </div>
+                    <div class="colorslider"></div>
+                    <div class="slider">
+                        <input type="range" min="1" max="5" name="star" >
+                    </div>
                     <div class="PaddingToelichtingReview">
                         Toelichting:<br><textarea rows="5" cols="70" placeholder="Vul hier uw toelichting in" name="ToelichtingReview" class="ToelichtingReview"> </textarea>
                     </div>
-                    <!-- <div class="rating">
-                        <input type="radio" name="star1" id="star1"><label for="star1">
-                        </label>
-                        <input type="radio" name="star2" id="star2"><label for="star2">
-                        </label>
-                        <input type="radio" name="star3" id="star3"><label for="star3">
-                        </label>
-                        <input type="radio" name="star4" id="star4"><label for="star4">
-                        </label>
-                        <input type="radio" name="star5" id="star5"><label for="star5">
-                        </label>
-                    </div> -->
-
-                    <div class="slider">
-                        <input type="range" min="1" max="5" id="star" name="star" value="3">
-                        <div id="Selector">
-                            <div class="SelectBtn"></div>
-                            <div id="SelectValue"></div>
-                        </div>
-                        <div id="ProgressBar"></div>
-                    </div>
-
-                    <script>
-                        var slider = document.getElementById("star");
-                        var selector = document.getElementById("selector");
-                        var SelectValue = document.getElementById("SelectValue");
-                        var ProgressBar = document.getElementById("ProgressBar");
-
-                        SelectValue.innerHTML = slider.value;
-
-                        slider.oninput = function(){
-                            SelectValue.innerHTML = this.value;
-                            selector.style.left = this.value + "%";
-                            ProgressBar.style.width = this.value + "%";
-                        }
-                    </script>
-
-
+                    <?php
+                    print_r($_POST); ?>
                     <input type="submit" value="Verstuur!" name="Verstuur" class="betaalbutton">
                 </form>
             </div>
@@ -243,17 +218,11 @@ if ($R) {
         ?><h2 id="ProductNotFound">Het opgevraagde product is niet gevonden.</h2><?php
     } ?>
 </div>
-
-
 <br>
-
 <?php
-
-
-
-$star = 0;
 $date = getdate();
 $ReviewID = "";
+$ToelichtingReview = "";
 
 foreach ($date as $time => $rv) {
 if ($time == "mday") {
@@ -269,7 +238,10 @@ break;
 }
 
 $ReviewDatum = "$year-$month-$day";
-print $ReviewDatum;
+print($ReviewDatum);
+
+
+
 
 ?>
 
@@ -278,24 +250,11 @@ $Query = "
 INSERT INTO reviewproduct
 VALUES (?, ?, ?, ?, ?)";
 
-// $Statement = mysqli_prepare($Connection, $Query);
-// mysqli_stmt_bind_param($Statement, "issis", $ReviewID, $_POST[OnderwerpReview],$_POST[ToelichtingReview],$star,$ReviewDatum);
-// mysqli_stmt_execute($Statement);
-// $R = mysqli_stmt_get_result($Statement);
-// ?>
+$Statement = mysqli_prepare($Connection, $Query);
+mysqli_stmt_bind_param($Statement, "issis", $ReviewID, $_POST['OnderwerpReview'],$_POST['ToelichtingReview'],$_POST['Star'],$ReviewDatum);
+mysqli_stmt_execute($Statement);
+$R = mysqli_stmt_get_result($Statement);
+?>
 <br>
-
-    <!-- <div class="rating">
-        <input type="radio" name="star1" id="star1"><label for="star1">
-        </label>
-        <input type="radio" name="star2" id="star2"><label for="star2">
-        </label>
-        <input type="radio" name="star3" id="star3"><label for="star3">
-        </label>
-        <input type="radio" name="star4" id="star4"><label for="star4">
-        </label>
-        <input type="radio" name="star5" id="star5"><label for="star5">
-        </label>
-    </div> -->
 
 
