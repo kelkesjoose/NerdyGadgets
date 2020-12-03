8cab306c9738ccc8e328f53c5d6fb52d487a4662
kelkesjoose <kaha-kelly@hotmail.com>
2020-12-03 16:19:35 +0100

    Order even wat mooier gemaakt :)

--- order.php
+++ order.php
@@ -17,42 +17,46 @@ if (empty($_SESSION["cart"])) {
 
 else {
 ?>
-  <div class="order">
-  </h5>
-    <div class="order">
-    <br>
-    <div class="besthead">
-    <h3>Vul hier uw bestelgegevens in!</h3>
-    </div><br>
-    <form method="get" action="ordercontrol.php">
+
+        </h5>
+        <br>
+<div class="order">
+        <h3>Vul hier uw bestelgegevens in!</h3>
+</div>
+    <div class="besthead"></div>
+            <div class="order">
+        <br>
+        <form method="get" action="ordercontrol.php">
+<!--        Bezorggegevens    -->
       <h3>Bezorggegevens</h3><br>
       <h5>Bezorging</h5>
         <h6>Waar wilt u het bezorgd hebben?</h6>
-        <div class="bezorg">
-          Thuis <input type="radio" id="thuis" name="bezwijze" value="Thuis" required><br>
-          Afhaalpunt <input type="radio" id="afhpunt" name="bezwijze" value="afhpunt" required><br><br>
+                  <input type="radio" id="thuis" name="bezwijze" value="Thuis" required> Thuis <br>
+          <input type="radio" id="afhpunt" name="bezwijze" value="afhpunt" required> Afhaalpunt <br><br>
         <h6>Op welke dag wil u het bezorgd hebben?</h6>
-          Zelfde dag <input type="radio" id="vandaag" name="tijd" value="vandaag" required ><br>
-          Morgen <input type="radio" id="morgen" name="tijd" value="morgen" required ><br><br>
-        </div>
+          <input type="radio" id="vandaag" name="tijd" value="vandaag" required > Zelfde dag <br>
+          <input type="radio" id="morgen" name="tijd" value="morgen" required > Morgen <br><br>
+<!--        Naam             -->
       <h5>Naam</h5>
         Voornaam:&#160;&#160;&#160;&#160;   <input type="text" id="fname" name="fname" required><br>
         Achternaam: &#160;<input type="text" id="lname" name="lname" required><br><br>
+<!--        Adresgegevens     -->
       <h5>Adresgegevens</h5>
         Postcode:&#160;&#160;&#160;&#160;&#160;&#160;&#160;<input type="text" id="pcode" name="pcode" required><br>
         Huisnummer: <input type="text" id="huisnr" name="huisnr" required><br>
-        e-mail:&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;     <input type="text" id="email" name="email" required><br><br><br><br>
-
-      <h3>Betaalwijze</h3>
+        e-mail:&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;<input type="text" id="email" name="email" required><br><br><br>
+<!--        Betaalwijze    -->
+      <h5>Betaalwijze</h5>
       <div class="betgeg">
-      Ideal <input type="radio" id="ideal" name="betwijze" value="ideal"><br>
-      Creditcard <input type="radio" id="cc" name="betwijze" value="cc"><br>
-      Overschrijving <input type="radio" id="overschr" name="betwijze" value="overschr"><br><br>
-      <input type="submit" value="Naar betaalpagina" name="submit" class="bestelbutton"><br></div></div>
+      <input type="radio" id="ideal" name="betwijze" value="ideal"> Ideal<br>
+       <input type="radio" id="cc" name="betwijze" value="cc"> Creditcard<br>
+       <input type="radio" id="overschr" name="betwijze" value="overschr"> Overschrijving<br><br></div></div>
+<!--      Bestelgegevens   -->
+            <div class="besthead"></div>
 <?php
 }
-
-print('<h2><div class="besthead">Uw Bestelling</div><br></h2><h5>');
+print("<div class='overzichtBestelling'><br>");
+print('<h4>Een overzicht van uw bestelling:<br></h4><h5>');
 foreach ($_SESSION["cart"] as $productnummer => $aantal) {
     $teller++;
 
@@ -65,13 +69,13 @@ foreach ($_SESSION["cart"] as $productnummer => $aantal) {
     mysqli_stmt_execute($Statement);
     $R = mysqli_stmt_get_result($Statement);
     $R = mysqli_fetch_all($R, MYSQLI_ASSOC);
-
+    print("<h6>");
     print ($R[0]["StockItemName"]);
     print ' | Aantal:';
     print " $aantal";
     print ' | Prijs: €';
     print round(($R[0]["SellPrice"]), 2) * $aantal;
-    print '<br>';
+
 
 
 
@@ -89,6 +93,10 @@ if ($totaalPrijs >= 25.00) {
     $subtotalen .= "<p class='subtotalen'>Totaalprijs: €" . round(($totaalPrijs + 4.95), 2) . "</p></td></tr>";
 }
 print $subtotalen;
+print("<br><input type='submit' value='Naar betaalpagina' name='submit' class='bestelbutton'><br>");
+print("</div>");
+
+print("</h6></div>");
+
 ?>
 
-</div>
